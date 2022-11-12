5b7042ba8a2d5252ae1a15163604adc28cd6f872
Gisela-Lopes <giselag_lopes@hotmail.com>
2022-11-11 22:08:58 -0300

    Tarefa Calculo Media mod8

---
+++ src/ExercícioMod8/OperadoresMod8.java
@@ -0,0 +1,63 @@
+package ExercícioMod8;
+
+/**
+ * @author Gisela
+ */
+public class OperadoresMod8 {
+
+    public static void main(String args[]) {
+     OperadoresMod8 operadoresMod8 = new OperadoresMod8();
+     primeiraParte();
+     acresentandoMaisUmaParte();
+
+    }
+     public static void primeiraParte() {
+
+      int num1 = 10;
+      int num2 = 20;
+      int num3 = 30;
+      int num4 = 40;
+
+      int num5 = 10 + 20;
+      int num6 = 30 + 40;
+
+      int num7 = (30 + 70) / 4;
+
+      System.out.println(num5);
+      System.out.println(num6);
+      System.out.println(num7);
+
+     }
+
+ public static void acresentandoMaisUmaParte() {
+     System.out.println("**** Parte 2 ****");
+
+     int num8 = 50;
+     int num9 = 60;
+     int num10 = 50 + 60;
+     System.out.println(num10);
+
+     int num11 = 70;
+     int num12 = 80;
+     int num13 = 70 + 80;
+     System.out.println(num13);
+
+     int num14 = 90;
+     int num15 = 100;
+     int num16 = 90 + 100;
+     System.out.println(num16);
+
+     int num17 = 110;
+     int num18 = 120;
+     int num19 = 110 + 120;
+     System.out.println(num19);
+
+     int num20 = (110 + 150 + 190 + 230)/ 4;
+     System.out.println(num20);
+
+
+
+ }
+}
+
+
